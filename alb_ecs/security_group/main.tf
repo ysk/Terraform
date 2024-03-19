499637000bbc879f17d8cc1c8438fb1afa38107e
YusukeIkeda <yusuke_aono@yahoo.co.jp>
2024-03-19 17:59:08 +0900

    iam role 作成中

--- alb_ecs/security_group/main.tf
+++ alb_ecs/security_group/main.tf
@@ -14,20 +14,20 @@ resource "aws_security_group" "default" {
 }
 
 resource "aws_security_group_rule" "ingress_example" {
-  type                  = "ingress"
-  from_port             = var.port
-  to_port               = var.port
-  protocol              = "tcp"
-  cidr_blocks           = var.cidr_blocks
+  type              = "ingress"
+  from_port         = var.port
+  to_port           = var.port
+  protocol          = "tcp"
+  cidr_blocks       = var.cidr_blocks
   security_group_id = aws_security_group.default.id
 }
 
 resource "aws_security_group_rule" "egress_example" {
-  type                  = "egress"
-  from_port             = "0"
-  to_port               = "0"
-  protocol              = "-1"
-  cidr_blocks           = ["0.0.0.0/0"]
+  type              = "egress"
+  from_port         = "0"
+  to_port           = "0"
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.default.id
 }
 
