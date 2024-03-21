10992766323222f496022bf01a3e060473610167
YusukeIkeda <yusuke_aono@yahoo.co.jp>
2024-03-21 17:21:57 +0900

    修正

--- s3_cloudfront/main.tf
+++ s3_cloudfront/main.tf
@@ -4,18 +4,24 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+  default_tags {
+    tags = {
+      Worker = "YusukeIkeda"
+      Date   = "2024/03/21"
+    }
+  }
 }
 
 ############################################################
 #### CloudFront
-resource "aws_cloudfront_distribution" "example" {
+resource "aws_cloudfront_distribution" "this" {
   enabled = true
   origin {
-    origin_id   = aws_s3_bucket.example.id
-    domain_name = aws_s3_bucket.example.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.this.id
+    domain_name = aws_s3_bucket.this.bucket_regional_domain_name
     //OAI(Origin Access Identity)の設定
     s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.example.cloudfront_access_identity_path
+      origin_access_identity = aws_cloudfront_origin_access_identity.this.cloudfront_access_identity_path
     }
   }
 
@@ -24,7 +30,7 @@ resource "aws_cloudfront_distribution" "example" {
   }
 
   default_cache_behavior {
-    target_origin_id       = aws_s3_bucket.example.id
+    target_origin_id       = aws_s3_bucket.this.id
     viewer_protocol_policy = "redirect-to-https"
     cached_methods         = ["GET", "HEAD"]
     allowed_methods        = ["GET", "HEAD"]
@@ -42,30 +48,38 @@ resource "aws_cloudfront_distribution" "example" {
       restriction_type = "none"
     }
   }
+
+  tags = {
+    Name = "${var.prefix}-${var.system_Name}-cf"
+  }
+
 }
 
 //OAIの作成
-resource "aws_cloudfront_origin_access_identity" "example" {}
+resource "aws_cloudfront_origin_access_identity" "this" {}
 
 
 
 ############################################################
 #### S3 Bucket
 
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "this" {
   bucket = "tf-s3cloudfront-static"
+  tags = {
+    Name = "${var.prefix}-${var.system_Name}-s3"
+  }
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket                  = aws_s3_bucket.example.id
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket                  = aws_s3_bucket.this.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_policy" "example" {
-  bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
   policy = data.aws_iam_policy_document.s3_main_policy.json
 }
 
@@ -74,10 +88,10 @@ data "aws_iam_policy_document" "s3_main_policy" {
   statement {
     principals {
       type        = "AWS"
-      identifiers = [aws_cloudfront_origin_access_identity.example.iam_arn]
+      identifiers = [aws_cloudfront_origin_access_identity.this.iam_arn]
     }
     actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.example.arn}/*"]
+    resources = ["${aws_s3_bucket.this.arn}/*"]
   }
 }
 
---
+++ s3_cloudfront/variables.tf
@@ -0,0 +1,30 @@
+############################################################
+#### parameters
+
+variable "system_Name" {
+  default = "SystemName"
+}
+
+variable "prefix" {
+  default = "dev"
+}
+
+variable "ami_id" {
+  default = "ami-0a211f4f633a3af5f"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "domain_name" {
+  default = "aws-manager.net"
+}
+
+variable "s3_bucket_name" {
+  default = "tf-s3cloudfront-static"
+}
+
+
+
+
