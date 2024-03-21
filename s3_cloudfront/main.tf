b4ed9294ed74fd57dabe7459cd210cbc2c13ec11
YusukeIkeda <yusuke_aono@yahoo.co.jp>
2024-03-21 11:02:58 +0900

    Provider configuration not presentというエラーが表示され、未解決

--- s3_cloudfront/main.tf
+++ s3_cloudfront/main.tf
@@ -1,29 +1,89 @@
-provider "aws" {
-  region = "ap-northeast-1"
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 3.28.0"
+    }
+  }
 }
 
+provider "aws" {
+  region = "us-east-1"
+}
 
-resource "aws_s3_bucket" "private" {
-  bucket = "tf-s3bucket-example"
-  versioning {
-    enabled = true
+############################################################
+#### CloudFront
+resource "aws_cloudfront_distribution" "example" {
+  enabled = true
+  origin {
+    origin_id   = aws_s3_bucket.example.id
+    domain_name = aws_s3_bucket.example.bucket_regional_domain_name
+    //OAI(Origin Access Identity)の設定
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.example.cloudfront_access_identity_path
+    }
   }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  default_cache_behavior {
+    target_origin_id       = aws_s3_bucket.example.id
+    viewer_protocol_policy = "redirect-to-https"
+    cached_methods         = ["GET", "HEAD"]
+    allowed_methods        = ["GET", "HEAD"]
+    forwarded_values {
+      query_string = false
+      headers      = []
+      cookies {
+        forward = "none"
       }
     }
   }
-}
 
-
-resource "aws_s3_bucket" "alb_log" {
-  bucket = "tf-alb-log-s3bucket-example"
-  lifecycle_rule {
-    enabled = true
-    expiration {
-      days = "180"
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
     }
   }
 }
+
+//OAIの作成
+resource "aws_cloudfront_origin_access_identity" "example" {}
+
+
+
+############################################################
+#### S3 Bucket
+
+resource "aws_s3_bucket" "example" {
+  bucket = "tf-s3cloudfront-static"
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket                  = aws_s3_bucket.example.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "example" {
+  bucket = aws_s3_bucket.example.id
+  policy = data.aws_iam_policy_document.s3_main_policy.json
+}
+
+data "aws_iam_policy_document" "s3_main_policy" {
+  //OAIからのアクセスのみ許可
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.example.iam_arn]
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.example.arn}/*"]
+  }
+}
+
